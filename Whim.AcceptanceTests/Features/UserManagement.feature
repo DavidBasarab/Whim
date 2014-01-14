2f0c62867c297fb7f4bec94490666ae33f3edeea
DBasarab <dbasarab617@gmail.com>
2014-01-13 23:05:45 -0500

    Was supposed to be in last commit.

---
+++ Whim.AcceptanceTests/Features/UserManagement.feature
@@ -0,0 +1,10 @@
+﻿Feature: UserManagement
+	In order to manage user of the system
+	As a system user
+	I want to be able to manage users, which inlcude create, retrevie, update, and delete
+
+Scenario: A user to the system
+	Given I have a user with first name of "Jack"
+    And I have a user with the last name of "Blood"
+	When I enter the user into the system
+	Then the user is created
---
+++ Whim.AcceptanceTests/StepDefinitions/HookUpSteps.cs
@@ -0,0 +1,17 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Whim.AcceptanceTests.StepDefinitions
+{
+    [Binding]
+    public class HookUpSteps
+    {
+        [Given(@"I need to hook up to testing")]
+        public void GivenINeedToHookUpToTesting() {}
+
+        [Then(@"I am hooked up and pass")]
+        public void ThenIAmHookedUpAndPass() {}
+
+        [When(@"I run a test")]
+        public void WhenIRunATest() {}
+    }
+}
